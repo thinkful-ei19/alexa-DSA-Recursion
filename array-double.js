0bf942086f1ef24b68b9b7c81687fc15103b45ea
Alexa Scott <arscott4792@gmail.com>
2018-05-14 12:10:40 -0700

    completed array double

--- array-double.js
+++ array-double.js
@@ -2,19 +2,37 @@
 
 // 2. Array Double
 
-let arr2 = [];
+// first attempt
+
+// let arr2 = [];
+// function double(arr) {
+//   if (arr.length === 0) {
+//     return;
+//   }
+
+//   const doubleNum = arr[0] * 2;
+//   //   console.log(doubleNum);
+//   arr2.push(doubleNum);
+//   console.log(arr2);
+//   return double(arr.slice(1));
+// }
+
+
+// let arr = [1, 2, 3];
+// double(arr);
+
+
+// second attempt
 function double(arr) {
   if (arr.length === 0) {
-    return;
+    return [];
   }
 
   const doubleNum = arr[0] * 2;
-  //   console.log(doubleNum);
-  arr2.push(doubleNum);
-  console.log(arr2);
-  return double(arr.slice(1));
+  
+  return [doubleNum, ...double(arr.slice(1))];
 }
 
 
 let arr = [1, 2, 3];
-double(arr);
+console.log(double(arr));
