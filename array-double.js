52ad401c210f890553c8172136b4010bfe216fd9
Alexa Scott <arscott4792@gmail.com>
2018-05-14 16:19:43 -0700

    completed iterative solution for arrayDouble

--- array-double.js
+++ array-double.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Recursive Solution
+
 // first attempt
 
 // let arr2 = [];
@@ -22,16 +24,32 @@
 
 // second attempt
 
-function double(arr) {
-  if (arr.length === 0) {
-    return [];
+// function double(arr) {
+//   if (arr.length === 0) {
+//     return [];
+//   }
+
+//   const doubleNum = arr[0] * 2;
+  
+//   return [doubleNum, ...double(arr.slice(1))];
+// }
+
+
+// let arr = [1, 2, 3];
+// console.log(double(arr));
+
+// Iterative Solution
+
+function double2(array) {
+  let doubledArray = [];
+  let num;
+  for(let i = 0; i < array.length; i++) {
+    num = array[i] * 2;
+    doubledArray.push(num);
   }
 
-  const doubleNum = arr[0] * 2;
-  
-  return [doubleNum, ...double(arr.slice(1))];
+  return doubledArray;
 }
 
-
 let arr = [1, 2, 3];
-console.log(double(arr));
+console.log(double2(arr));
