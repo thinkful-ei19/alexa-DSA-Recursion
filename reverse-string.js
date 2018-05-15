7d8ee2dd483d5d0bb06811a23c9a5b8fc1fc8330
Alexa Scott <arscott4792@gmail.com>
2018-05-14 17:23:35 -0700

    completed iterative solution for reverseString

--- reverse-string.js
+++ reverse-string.js
@@ -1,33 +1,54 @@
 'use strict';
 
+// Recursive Solution
 
 // my first solution
 
-function reverse(str) {
-  if (str === '') {
-    return '';
-  }
+// function reverse(str) {
+//   if (str === '') {
+//     return '';
+//   }
   
-  const lastChar = str[str.length - 1];
+//   const lastChar = str[str.length - 1];
     
-  return lastChar + reverse(str.slice(0, -1));
+//   return lastChar + reverse(str.slice(0, -1));
+// }
+  
+  
+// console.log(reverse('hello'));
+
+
+// //second solution
+
+// function reverse2(str) {
+//   if (str === '') {
+//     return '';
+//   }
+    
+//   const newChar = str[0];
+    
+//   return reverse2(str.slice(1)) + newChar;
+// }
+    
+// console.log(reverse2('hello'));
+
+
+// Iterative Solution
+
+function reverse3(string) {
+  const strArr = string.split('');
+//   console.log(strArr);
+
+  const reverseArr = strArr.reverse();
+//   console.log(strArr);
+
+  const joinArr = reverseArr.join('');
+//   console.log(joinArr);
+
+  return joinArr;
 }
-  
-  
-console.log(reverse('hello'));
+
+console.log(reverse3('hello'));
 
 
-//second solution
-
-function reverse2(str) {
-  if (str === '') {
-    return '';
-  }
-    
-  const newChar = str[0];
-    
-  return reverse2(str.slice(1)) + newChar;
-}
-    
-console.log(reverse2('hello'));
   
